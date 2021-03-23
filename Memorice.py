d0ef8a7174935012f2e7f4bb5e188fd40db781b8
Cristobal <cjmoore@miuandes.cl>
2021-03-23 10:50:06 -0300

    second commit

--- Memorice.py
+++ Memorice.py
@@ -1,15 +1,22 @@
 import random
-from random import randint
+import numpy as np
+from random import randint, sample
 
-Nummber_of_cards = input("Please insert the number of cards you wish to play with: ")
-cards = int(Nummber_of_cards)
-player1 = 0
-player2 = 0
-tablero = list()
 
-for i in range(cards):
-    r1 = random.randint(1,cards)
-    r2 = random.randint(1,cards)
-    tablero.append(r1)
-    tablero.append(r2)
+Cards = int(input("Please insert the amount of cards you wish to play with: "))
 
+
+numbers = range(1,Cards+1)
+b = sample(numbers, k=Cards)
+a = sample(numbers, k=Cards)
+#print (b)
+#print (a)
+
+arr1 = np.array(b)
+arr2 = np.array(a)
+
+#print (arr1)
+#print (arr2)
+
+matrix = np.array([arr1, arr2])
+print (matrix)
