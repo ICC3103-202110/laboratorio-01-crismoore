be3d837dd4fc33c4c09cea6c852750ee76e9fc2c
Cristobal <cjmoore@miuandes.cl>
2021-03-25 11:12:15 -0300

    sixth commit

--- Memorice.py
+++ Memorice.py
@@ -25,29 +25,89 @@ def createMatrix2(card):
     matrix2 = np.array([row3, row4])
     return matrix2
 
-def replace_values(card):
+def replace_values(card, board, board2):
     print ("The origin coordenate of the board is on the top left corner")
     x = int(input("Please insert coordenate in x: "))
     if x>=0 and x<card:
         y = int(input("Please insert coordenate in y between -1 and 0: "))
         if y == 0 or y ==-1:
-            Board = createMatrix(Cards)
-            Board2 = createMatrix2(Cards)
-            Board2[y][x] = Board[y][x]
-            print (Board)
-            return (Board2)
+            board2[y][x] = board[y][x]
+            print (board)
+            return (board2)
         else:
-            print ("You inserted the wrong coordenate")
-            
+            print ("You inserted a coordenate out of range")
+    else: 
+        print ("You inserted a coordenate out of range")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Cards = int(input("Please insert the amount of cards you wish to play with: "))
 
 Player1 = 0
 Player2 = 0
 
-Replace = replace_values(Cards)
+Board = createMatrix(Cards)
+Board2 = createMatrix2(Cards)
+print (Board)
+print (Board2)
+print ("The origin coordenate of the board is on the top left corner")
 
-print (Replace)
+contador=0
+value1 = 0
+value2 = 0
+#while contador<(Cards*2):
+i = 0
+while i<2:
+    x = int(input("Please insert coordenate in x: "))
+    if x>=0 and x<Cards:
+        y = int(input("Please insert coordenate in y between -1 and 0: "))
+        if y == 0 or y ==-1:
+            value1=Board[y][x]
+            Board2[y][x] = Board[y][x]
+            print(Board2)
+            if value1 == 0:
+                value1=Board[y][x]
+                i+=1
+            else:
+                value2=Board[y][x]
+                i+=1
+        else:
+            print ("You inserted a coordenate out of range")
+    else: 
+        print ("You inserted a coordenate out of range")
+        
+print(value1)
+print(value2)      
+        
+        
+        
+        
+#Replace = replace_values(Cards, Board, Board2)
+#print (Replace)
+#i+=1
+        
+
+
+
+
+    
+    
+
 
 
 #print (Board)
