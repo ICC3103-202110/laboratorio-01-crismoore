8a954a818fbce257858307bc0571399bdf1ee235
Cristobal <cjmoore@miuandes.cl>
2021-03-31 16:32:41 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from random import sample
+
+from coordenates_board import Coordenates_board
+#from board import Board
+
+
+
+def main():
+
+    print ("\nWelcome to the Memorice game made by Cristobal Moore")
+    Amount_of_cards = int(input("Please insert the amount of pairs of cards you wish to play with: "))
+
+
+if __name__ == "__main__" :
+    main()
